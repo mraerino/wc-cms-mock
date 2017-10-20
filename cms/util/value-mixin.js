5390acc36f449ad9f89881bed84a4819b59f21d2
Marcus Weiner <marcus.weiner@gmail.com>
2017-10-20 19:30:31 +0200

    :hammer: Refactor value attrbute into mixin

---
+++ cms/util/value-mixin.js
@@ -0,0 +1,33 @@
+export default function(superClass) {
+    class ValueMixin extends superClass {
+        constructor() {
+            super();
+
+            this._value = "";
+        }
+
+        static get observedAttributes() {
+            return (super.observedAttributes || []).concat(['value']);
+        }
+
+        attributeChangedCallback(attrName, oldVal, newVal) {
+            if(attrName === 'value') {
+                this[attrName] = newVal;
+                return;
+            }
+
+            return super.attributeChangedCallback(attrName, oldVal, newVal);
+        }
+
+        set value(val) {
+            this._value = val;
+        }
+
+        get value() {
+            return this._value;
+        }
+
+    }
+
+    return valueMixin;
+}
--- cms/wcms-value.js
+++ cms/wcms-value.js
@@ -1,37 +1,19 @@
 import get from '../node_modules/lodash-es/get.js';
 import KeyMixin from './util/key-mixin.js';
 import ContentMixin from './util/content-mixin.js';
+import ValueMixin from './util/value-mixin.js';
 
-export class WcmsValue extends ContentMixin(KeyMixin(HTMLElement)) {
+export class WcmsValue extends ValueMixin(ContentMixin(KeyMixin(HTMLElement))) {
 
     constructor() {
         super();
 
-        this._value = "";
-
         this.addEventListener('content-changed', () => this.fetchValue());
     }
 
-    static get observedAttributes() {
-        return (super.observedAttributes || []).concat(['value']);
-    }
-
-    attributeChangedCallback(attrName, oldVal, newVal) {
-        if(attrName === 'value') {
-            this[attrName] = newVal;
-            return;
-        }
-
-        return super.attributeChangedCallback(attrName, oldVal, newVal);
-    }
-
     set value(val) {
-        this._value = val;
+        super.value = val;
         this.textContent = this._value;
-    }
-
-    get value() {
-        return this._value;
     }
 
     set key(val) {
